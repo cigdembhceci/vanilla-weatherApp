ca2f2444c95d52f0d5e1b7d752f30f0de54b83bf
cigdembhceci <cigdembhceci@hotmail.com>
2022-05-11 19:08:40 -0400

    Update apiUrl

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ function displaytemp(response) {
 }
 
 let apiKey = "a70f9c31d69f5263e3e2bfe53953ae61";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=imperial`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
 
 axios.get(`${apiUrl}&appid=${apiKey}`).then(displaytemp);
