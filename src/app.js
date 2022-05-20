2edb4de0269a4258369e45387a488dfaf5341fa0
cigdembhceci <cigdembhceci@hotmail.com>
2022-05-19 20:14:51 -0400

    added search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,18 @@ function displaytemp(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "a70f9c31d69f5263e3e2bfe53953ae61";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+function search(city) {
+  let apiKey = "a70f9c31d69f5263e3e2bfe53953ae61";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displaytemp);
+}
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(displaytemp);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
